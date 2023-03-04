66cc4a9771f93928dc7f3044f95def62c58fd15b
johljohl <joohan.lindell@gmail.com>
2023-03-04 13:32:36 +0100

    Added some comments

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,22 +1,27 @@
 import React from "react";
 
+// A functional component that renders a modal with product information.
 const Modal = (props) => {
+  // Destructuring is used to extract the product object and closeModal function from props.
   const { product, closeModal } = props;
+  // Destructuring is also used to extract specific properties from the product object, such as name, description, price, image, and about.
   const { name, description, price, image, about } = product;
+  // Renders a modal component that displays information about the product.
   return (
     <div className="modal">
       <div className="modal-content">
         <img className="modal-image" src={image} alt={name} />
         <h2>{name}</h2>
         <p>{description}</p>
-        <p>Pris {price} kr</p>
+        <p>Price: {price} kr</p>
         <p>{about}</p>
         <button className="close-button" onClick={closeModal}>
-          Stäng
+          Close
         </button>
       </div>
     </div>
   );
 };
 
+// Exports the Modal component to be used in other parts of the application.
 export default Modal;
--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import Modal from "./Modal";
 
+// A functional component that displays search results for a product.
 const SearchResult = (props) => {
-  const { product, addToCart } = props;
+  // Destructuring is used to extract the product object and addToShoppingCart function from props.
+  const { product, addToShoppingCart } = props;
+  // State is used to track whether or not the modal should be displayed.
   const [showModal, setShowModal] = useState(false);
 
+  // Event handlers for the "Add to cart" and "More information" buttons.
   const addToCartClick = () => {
     addToShoppingCart();
   };
@@ -15,16 +19,20 @@ const SearchResult = (props) => {
     setShowModal(false);
   };
 
+  // Renders a card that displays information about the product.
   return (
     <div className="card">
       <h3>{product.name}</h3>
       <img src={product.image} alt={product.name} />
       <p>{product.description}</p>
-      <p>Pris {product.price} kr</p>
-      <button onClick={addToCartClick}>Lägg till i kundvagn</button>
-      <button onClick={showModalClick}>Mer information</button>
+      <p>Price: {product.price} kr</p>
+      <button onClick={addToCartClick}>Add to cart</button>
+      <button onClick={showModalClick}>More information</button>
+      {/* Renders a Modal component if showModal is true. */}
       {showModal && <Modal product={product} closeModal={closeModalClick} />}
     </div>
   );
 };
+
+// Exports the SearchResult component to be used in other parts of the application.
 export default SearchResult;
